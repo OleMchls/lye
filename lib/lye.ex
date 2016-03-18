e38bf926534e7bc83b0cfffbd508bf1656ea956a
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-03-18 11:25:14 +0100

    use ranch directly

--- lib/lye.ex
+++ lib/lye.ex
@@ -1,8 +1,25 @@
 defmodule Lye do
   use Application
 
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
   def start(_type, _args) do
-    :ranch.start_listener(:lye, 100,
+    import Supervisor.Spec, warn: false
+
+    children = [
+      ranch
+      # Define workers and child supervisors to be supervised
+      # worker(Banana.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Lye.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def ranch do
+    :ranch.child_spec(:lye, 100,
       :ranch_ssl, [
         {:port, Application.get_env(:lye, :port)},
         {:certfile, Application.get_env(:lye, :certfile)},
